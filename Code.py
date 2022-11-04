c546aa82837beb2c17b73e0ca39744a52e0e8072
yulin09 <yulintyandra09@gmail.com>
2022-11-04 09:11:19 +0700

    Menu

---
+++ Code.py
@@ -0,0 +1,8 @@
+print ('WELCOME TO GIS 1 ATM')
+print ('PILIH OPTION')
+print ('1. Check uang nasabah')
+print ('2. Ambil uang')
+print ('3. Tabung uang')
+option=int(input('silahkan pilih option : '))
+uang_kamu=200000
+if option==1:
