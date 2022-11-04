2483eb6defeed02eae129ee42b70d66a729e68c9
Fellica <fellicaclaracindy@gmail.com>
2022-11-04 12:31:54 +0700

    fellica

--- Code.py
+++ Code.py
@@ -12,7 +12,7 @@ elif option==2:
   print('1. Rp.100.000')
   print('2. Rp.200.000')
   option2=int(input('option:'))
-  if option2==1:
+if option2==1:
     hasil=uang_kamu-100000
     print('uang kamu sekarang berjumlah ',hasil)
 elif option2==2:
@@ -20,11 +20,11 @@ elif option2==2:
     print('uang kamu sekarang berjumlah ',hasil2)
 else:
     print('keyboard anda salah, mohon ulangi lagi!')
-elif option==3:
-    print(uang anda berjumlah Rp.200.000, mau tabung berapa?)
+if option==3:
+    print('uang anda berjumlah Rp.200.000, mau tabung berapa?')
     option3=int(input('masukan jumlah uang:'))
     hasil3=uang_kamu+option3
     print('jumlah uang kamu sekarang adalah',hasil3)
 else:
     print("pilihan anda salah, silahkan ulangi lagi!!!")
-    
+exit()
